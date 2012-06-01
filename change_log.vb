c545fe9f88a9c703a8479404e5fc50450c9302be
Steven Oliver <oliver.steven@gmail.com>
2012-06-01 14:17:23 -0300

    Update copyright header

--- change_log.vb
+++ change_log.vb
@@ -1,4 +1,4 @@
-'Copyright 2011 Steven Oliver <oliver.steven@gmail.com>
+'Copyright 2011, 2012 Steven Oliver <oliver.steven@gmail.com>
 '
 'Licensed under the Apache License, Version 2.0 (the "License");
 'you may not use this file except in compliance with the License.
